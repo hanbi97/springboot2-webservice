267057f17e490eae82d1567aaef2aadfdfaaa08c
hanbi <rain7i@naver.com>
2020-07-19 00:22:14 +0900

    [Chapter 3] add JPA auditing

--- src/main/java/com/practice/springboot/Application.java
+++ src/main/java/com/practice/springboot/Application.java
@@ -2,7 +2,9 @@ package com.practice.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing //JPA auditing
 @SpringBootApplication
 public class Application { //main class
     public static void main(String[] args){
